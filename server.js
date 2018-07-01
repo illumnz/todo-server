0b31fb708d9c6e6d88dc19a7f7939cce37c88d1e
illumnz <thanakritnlc@gmail.com>
2018-07-01 13:48:51 +0700

    todo-server

---
+++ server.js
@@ -0,0 +1,11 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db/db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+var port = process.env.PORT || 3000;
+server.listen(port, () => {
+ console.log("JSON Server is running");
+});
